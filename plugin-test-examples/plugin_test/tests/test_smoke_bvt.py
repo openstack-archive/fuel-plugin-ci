ff69508da9eae9278dabe54fd0a3378096194a53
Alexey Elagin <aelagin@mirantis.com>
2015-12-18 03:18:03 +0300

    change test_smoke_bvt.py
    Change-Id: I9f721e6ad42e06791afe20207d37226ccd650ba0

--- plugin-test-examples/plugin_test/tests/test_smoke_bvt.py
+++ plugin-test-examples/plugin_test/tests/test_smoke_bvt.py
@@ -30,7 +30,7 @@ class TestPlugin(TestBasic):
 
     cluster_id = ''
 
-    @test(depends_on=[SetupEnvironment.prepare_slaves_3],
+    @test(depends_on=[SetupEnvironment.prepare_slaves_2],
           groups=["install_testplugin"])
     @log_snapshot_after_test
     def install_testplugin(self):
@@ -47,14 +47,43 @@ class TestPlugin(TestBasic):
 
         """
 
-        plugin.prepare_test_plugin(self, slaves=3)
+        plugin.prepare_test_plugin(self, slaves=2)
 
-    @test(depends_on=[SetupEnvironment.prepare_slaves_3],
+    @test(depends_on=[SetupEnvironment.prepare_slaves_2],
           groups=["plugin_smoke"])
     @log_snapshot_after_test
-   
-    self.fuel_web.run_ostf(
-        cluster_id=self.cluster_id,
-        should_fail=2,
-        failed_test_name=[('Check network connectivity from instance via floating IP'),('Launch instance with file injection')]
-    )
+
+    def plugin_smoke(self):
+        """Deploy a cluster with Plugin
+
+        Scenario:
+            1. Revert snapshot "ready_with_2_slaves"
+            2. Create cluster
+            3. Add a node with controller role
+            4. Add a node with compute role
+            6. Enable Contrail plugin
+            5. Deploy cluster with plugin
+
+        Duration 90 min
+
+        """
+        plugin.prepare_test_plugin(self, slaves=2)
+
+        # enable plugin in settings
+        plugin.activate_plugin(self)
+
+        self.fuel_web.update_nodes(
+            self.cluster_id,
+            {
+                'slave-01': ['controller'],
+                'slave-02': ['compute'],
+            })
+
+        # deploy cluster
+        openstack.deploy_cluster(self)
+
+        self.fuel_web.run_ostf(
+            cluster_id=self.cluster_id,
+            should_fail=2,
+            failed_test_name=[('Check network connectivity from instance via floating IP'),('Launch instance with file injection')]
+        )
